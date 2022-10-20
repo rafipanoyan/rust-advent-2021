fd5b3723a9c4b1d6b8b248fa1fbb689488889a2f
Rafi Panoyan <rafi.panoyan@hotmail.fr>
2022-10-21 00:42:55 +0200

    from IntoIterator to Array
    day3 done
    
    day 3 really done

--- src/past/day_3/day_3_part_2.rs
+++ src/past/day_3/day_3_part_2.rs
@@ -1,63 +1,71 @@
-fn day_3_part_2() {
-    let input: Vec<String> = INPUT.split('\n').map(|s| s.trim().to_owned()).collect();
+mod data;
 
-    let oxygen_word = get_oxygen_generator_rating(&input, 0, &|digit_count: DigitCount| -> char {
-        if digit_count.0 > digit_count.1 {
+use crate::data::data::INPUT;
+
+fn day_3_part_2() {
+    let input: Vec<&str> = INPUT.split('\n').map(|s| s.trim()).collect();
+
+    let oxygen_bit_chooser = |dc: DigitCount| {
+        if dc.0 > dc.1 {
             '0'
         } else {
             '1'
         }
-    }).unwrap();
-    let oxygen = u16::from_str_radix(&oxygen_word, 2).unwrap();
-    println!("Oxygen Generator Rating: {}", oxygen);
-
-    let co2_word = get_oxygen_generator_rating(&input, 0, &|digit_count: DigitCount| -> char {
+    };
+    let co2_bit_chooser = |digit_count: DigitCount| -> char {
         if digit_count.0 <= digit_count.1 {
             '0'
         } else {
             '1'
         }
-    }).unwrap();
-    let co2 = u16::from_str_radix(&co2_word, 2).unwrap();
-    println!("CO2 Scubber rating: {}", co2);
+    };
 
+    let oxygen_word = get_rating(&input, 0, &oxygen_bit_chooser).unwrap();
+    let oxygen = u16::from_str_radix(&oxygen_word, 2).unwrap();
+
+    let co2_word = get_rating(&input, 0, &co2_bit_chooser).unwrap();
+    let co2 = u16::from_str_radix(&co2_word, 2).unwrap();
+    
     let life_support_rating: f64 = <u16 as Into<f64>>::into(oxygen) * <u16 as Into<f64>>::into(co2);
+    
+    println!("Oxygen Generator Rating: {}", oxygen);
+    println!("CO2 Scubber rating: {}", co2);
     println!("Life Support rating: {}", life_support_rating);
 }
 
 struct DigitCount(u32, u32);
 
-fn get_oxygen_generator_rating(
-    input: &Vec<String>,
+fn get_rating(
+    input: &[&str],
     position: usize,
-    mapper: &dyn Fn(DigitCount) -> char,
+    bit_chooser: &dyn Fn(DigitCount) -> char,
 ) -> Option<String> {
     if input.len() == 1 {
         return match input.first() {
-            Some(value) => Option::Some(value.to_owned()),
+            Some(&value) => Option::Some(value.to_owned()),
             _ => Option::None,
         };
     }
 
     let count = get_digit_count(input, position);
-    let discriminant = mapper(count);
+    let discriminant = bit_chooser(count);
     let new_input = input
         .iter()
         .filter(|&word| match word.chars().nth(position) {
             Some(bit) => bit == discriminant,
             _ => false,
         })
-        .map(|word| word.to_owned())
-        .collect::<Vec<String>>();
+        .map(|&word| word)
+        .collect::<Vec<&str>>();
 
     println!("Position {}: {} element left.", position, new_input.len());
 
-    return get_oxygen_generator_rating(&new_input, position + 1, mapper);
+    return get_rating(&new_input, position + 1, bit_chooser);
 }
 
-fn get_digit_count(input: &Vec<String>, position: usize) -> DigitCount {
+fn get_digit_count(input: &[&str], position: usize) -> DigitCount {
     input
-        .iter()
+        .into_iter()
         .map(|word| -> u32 { get_digit_at(word, position) })
         .fold(DigitCount(0, 0), |acc, d| match d {
             0 => DigitCount(acc.0 + 1, acc.1),
