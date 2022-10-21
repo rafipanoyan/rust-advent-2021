4ec23c390779258303c1408b88793c7d72630d0a
Rafi Panoyan <rafi.panoyan@hotmail.fr>
2022-10-21 02:21:01 +0200

    parse data

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,98 @@
 mod data;
 
+use std::fmt::{Display, write, format};
+
 use crate::data::data::INPUT;
 
-fn main() {
-    let input: Vec<&str> = INPUT.split('\n').map(|s| s.trim()).collect();
-
-    let oxygen_bit_chooser = |dc: DigitCount| {
-        if dc.0 > dc.1 {
-            '0'
-        } else {
-            '1'
-        }
-    };
-    let co2_bit_chooser = |digit_count: DigitCount| -> char {
-        if digit_count.0 <= digit_count.1 {
-            '0'
-        } else {
-            '1'
-        }
-    };
-
-    let oxygen_word = get_rating(&input, 0, &oxygen_bit_chooser).unwrap();
-    let oxygen = u16::from_str_radix(&oxygen_word, 2).unwrap();
-
-    let co2_word = get_rating(&input, 0, &co2_bit_chooser).unwrap();
-    let co2 = u16::from_str_radix(&co2_word, 2).unwrap();
-    
-    let life_support_rating: f64 = <u16 as Into<f64>>::into(oxygen) * <u16 as Into<f64>>::into(co2);
-    
-    println!("Oxygen Generator Rating: {}", oxygen);
-    println!("CO2 Scubber rating: {}", co2);
-    println!("Life Support rating: {}", life_support_rating);
+#[derive(Copy, Clone)]
+struct Cell {
+    value: i32,
+    marked: bool,
 }
 
-struct DigitCount(u32, u32);
-
-fn get_rating(
-    input: &[&str],
-    position: usize,
-    bit_chooser: &dyn Fn(DigitCount) -> char,
-) -> Option<String> {
-    if input.len() == 1 {
-        return match input.first() {
-            Some(&value) => Option::Some(value.to_owned()),
-            _ => Option::None,
-        };
+impl Cell {
+    fn new(value: i32) -> Cell {
+        Cell {
+            value,
+            marked: false,
+        }
     }
-
-    let count = get_digit_count(input, position);
-    let discriminant = bit_chooser(count);
-    let new_input = input
-        .iter()
-        .filter(|&word| match word.chars().nth(position) {
-            Some(bit) => bit == discriminant,
-            _ => false,
-        })
-        .map(|&word| word)
-        .collect::<Vec<&str>>();
-
-    println!("Position {}: {} element left.", position, new_input.len());
-
-    return get_rating(&new_input, position + 1, bit_chooser);
 }
 
-fn get_digit_count(input: &[&str], position: usize) -> DigitCount {
-    input
-        .into_iter()
-        .map(|word| -> u32 { get_digit_at(word, position) })
-        .fold(DigitCount(0, 0), |acc, d| match d {
-            0 => DigitCount(acc.0 + 1, acc.1),
-            1 => DigitCount(acc.0, acc.1 + 1),
-            _ => acc,
-        })
+impl Display for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mark = match self.marked {
+            true => '✅',
+            false => '⭕'
+        };
+        write!(f, "{:02}:{} ", self.value, mark)
+    }
 }
 
-fn get_digit_at(word: &str, position: usize) -> u32 {
-    let char_at = word.chars().nth(position);
-    char_at.unwrap_or('0').to_digit(10).unwrap_or(0)
+impl FromIterator<Cell> for [Cell; 5] {
+    fn from_iter<T: IntoIterator<Item = Cell>>(iter: T) -> Self {
+        let mut row = [Cell::new(0); 5];
+
+        iter.into_iter().take(5).enumerate().for_each(|(i, cell)| {
+            row[i] = cell;
+        });
+        row
+    }
+}
+
+struct Board {
+    matrix: [[Cell; 5]; 5]
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            matrix: [[Cell::new(0); 5]; 5],
+        }
+    }
+}
+
+impl Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut display = String::new();
+        for row in self.matrix {
+            for cell in row {
+                let cell_str = format!("{cell}");
+                display.push_str(&cell_str);
+            }
+            display.push('\n')
+        }
+        write!(f, "{}", display)
+    }
+}
+
+fn main() {
+    let mut input = INPUT.split("\n\n").into_iter();
+    let random_numbers: &str = input.next().unwrap();
+    let random_numbers: Vec<&str> = random_numbers.split(',').collect();
+
+    let boards: Vec<Board> = input.map(|board_str| {
+        let rows = board_str.split('\n').into_iter();
+        rows.map(|row| {
+            let numbers = row.split_whitespace();
+            let array = numbers
+                .take(5)
+                .map(|number| Cell::new(number.parse().unwrap()))
+                .collect::<[Cell; 5]>();
+                array
+        })
+        .enumerate()
+        .fold(
+            Board::new(),
+            |mut board: Board, (index, row)| {
+                board.matrix[index] = row;
+                board
+            },
+        )
+    })
+    .collect();
+
+    for b in boards {
+        println!("{}", b);
+    }
 }
